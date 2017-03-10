3e8405e223360747d46df026d24f938f1bfee699
Giggs3244 <bryan.bedoy@gmail.com>
2017-03-10 16:40:16 -0500

    Third Commit

---
+++ jersey-file-upload-download-mysql/src/main/java/com/giggs/test/Test.java
@@ -0,0 +1,13 @@
+package com.giggs.test;
+
+import com.giggs.util.LoggerUtil;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		LoggerUtil.info(Test.class.getName(), "Dentro del metodo main");
+		
+	}
+
+}
--- jersey-file-upload-download-mysql/src/main/java/com/giggs/util/LoggerUtil.java
+++ jersey-file-upload-download-mysql/src/main/java/com/giggs/util/LoggerUtil.java
@@ -1,7 +1,34 @@
 package com.giggs.util;
 
+import org.apache.log4j.Logger;
+
 public class LoggerUtil {
-	
-	
+
+	private LoggerUtil() {
+	}
+
+	public static void info(String clase, String mensaje) {
+		Logger logger = Logger.getLogger(clase);
+		int lineNumber = Thread.currentThread().getStackTrace()[2].getLineNumber();
+		String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
+		logger.info("[" + lineNumber + "] [" + methodName + "] [" + mensaje + "]");
+	}
+
+	// public static void info(String clase, String mensaje) {
+	// String fullClassName =
+	// Thread.currentThread().getStackTrace()[2].getClassName();
+	// String className = fullClassName.substring(fullClassName.lastIndexOf(".")
+	// + 1);
+	// String methodName =
+	// Thread.currentThread().getStackTrace()[2].getMethodName();
+	// int lineNumber =
+	// Thread.currentThread().getStackTrace()[2].getLineNumber();
+	//
+	// Logger logger = Logger.getLogger(clase);
+	// // logger.info(mensaje);
+	//
+	// logger.info(className + "." + methodName + "():" + lineNumber + "- " +
+	// mensaje);
+	// }
 
 }
